d6028f58c34887da64ba4e5ec9b0f9972b4b9d4e
Nylah <nylah@nanoomenergy.com>
2023-09-08 16:50:20 +0900

    no message

--- src/main/java/me/seonyeong/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/seonyeong/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -1,2 +1,11 @@
-package me.seonyeong.springbootdeveloper;public class SpringBootDeveloperApplication {
+package me.seonyeong.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
 }
